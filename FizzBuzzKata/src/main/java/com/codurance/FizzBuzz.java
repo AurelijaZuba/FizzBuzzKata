c8ce9ece974ce8c7d0ae22999ffc221051a32d57
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 11:54:55 +0100

    Return FizzBuzz for input of 15

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -6,6 +6,8 @@ public class FizzBuzz {
     public static final String BUZZ = "Buzz";
 
     public String calculate(int number) {
+        if(number % 5 == 0 && number % 3 == 0)
+            return "FizzBuzz";
         if(number % 5 == 0)
             return BUZZ;
         if(number % 3 == 0)
