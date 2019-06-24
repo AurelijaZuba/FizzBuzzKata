9f471425798003ff3ec636aace5c00ab4d07cd0e
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 11:56:33 +0100

    Extract method isMultipleOf3

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -6,13 +6,17 @@ public class FizzBuzz {
     public static final String BUZZ = "Buzz";
 
     public String calculate(int number) {
-        if(isMultipleOf5(number) && number % 3 == 0)
+        if(isMultipleOf5(number) && isMultipleOf3(number))
             return "FizzBuzz";
         if(isMultipleOf5(number))
             return BUZZ;
-        if(number % 3 == 0)
+        if(isMultipleOf3(number))
             return FIZZ;
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
     }
 
     private boolean isMultipleOf5(int number) {
