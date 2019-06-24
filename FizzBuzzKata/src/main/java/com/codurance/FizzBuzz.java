64fcd06bfa0ec13ac1a552181e7aaa1b871b89e0
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 11:55:57 +0100

    Extract method isMultipleOf5

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -6,12 +6,16 @@ public class FizzBuzz {
     public static final String BUZZ = "Buzz";
 
     public String calculate(int number) {
-        if(number % 5 == 0 && number % 3 == 0)
+        if(isMultipleOf5(number) && number % 3 == 0)
             return "FizzBuzz";
-        if(number % 5 == 0)
+        if(isMultipleOf5(number))
             return BUZZ;
         if(number % 3 == 0)
             return FIZZ;
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
     }
 }
