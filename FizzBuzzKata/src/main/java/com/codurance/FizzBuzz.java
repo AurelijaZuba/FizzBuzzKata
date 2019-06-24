75f58794cd76bcccd2a16b07db540e7119cbd622
AurelijaZuba <auryte00@hotmail.com>
2019-06-24 11:50:39 +0100

    Refactor to extract constant Buzz

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -3,10 +3,11 @@ package com.codurance;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String calculate(int number) {
         if(number % 5 == 0)
-            return "Buzz";
+            return BUZZ;
         if(number % 3 == 0)
             return FIZZ;
         return String.valueOf(number);
