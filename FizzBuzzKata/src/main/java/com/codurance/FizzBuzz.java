6880915a6b9cc940a3e9f52c71d66a4f24848311
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 11:18:19 +0100

    Input of 4 returns '4'

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.codurance;
 
 public class FizzBuzz {
     public String calculate(int number) {
+        if(number == 4)
+            return "4";
         if(number == 2)
             return "2";
         return "1";
