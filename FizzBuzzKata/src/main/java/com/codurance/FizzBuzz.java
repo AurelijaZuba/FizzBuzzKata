a4eec9047302b1606507b09f9e8fbc91aa8d0d79
AurelijaZuba <auryte00@hotmail.com>
2019-06-24 10:54:56 +0100

    failing test for integer 1

---
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.codurance;
+
+public class FizzBuzz {
+    public String calculate(int i) {
+        throw new UnsupportedOperationException();
+    }
+}
--- FizzBuzzKata/src/test/java/com/codurance/FizzBuzzShould.java
+++ FizzBuzzKata/src/test/java/com/codurance/FizzBuzzShould.java
@@ -1,4 +1,16 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzShould {
+
+
+    @Test
+    void return_one_when_passing_one() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertThat(fizzBuzz.calculate(1)).isEqualTo(1);
+    }
 }
