a183600b0b3fe2c48228fb694e96c57a510da3d0
AurelijaZuba <auryte00@hotmail.com>
2019-06-24 11:47:28 +0100

    Passing test with obvious implementation for multiples of 5

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
 
     public String calculate(int number) {
+        if(number % 5 == 0)
+            return "Buzz";
         if(number % 3 == 0)
             return FIZZ;
         return String.valueOf(number);
