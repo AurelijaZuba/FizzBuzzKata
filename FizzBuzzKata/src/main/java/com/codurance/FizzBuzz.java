5f212513fb8915e42977327684396d5926bb9072
AurelijaZuba <auryte00@hotmail.com>
2019-06-24 11:31:07 +0100

    Passing test for Fizz when input is 3

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.codurance;
 
 public class FizzBuzz {
     public String calculate(int number) {
+        if(number == 3)
+            return "Fizz";
         return String.valueOf(number);
     }
 }
