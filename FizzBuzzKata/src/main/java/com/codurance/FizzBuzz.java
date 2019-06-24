6f7c4c11f8866d24d33e596ba13033fce6fcab95
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 11:20:45 +0100

    Return string value of number

--- FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
+++ FizzBuzzKata/src/main/java/com/codurance/FizzBuzz.java
@@ -2,10 +2,6 @@ package com.codurance;
 
 public class FizzBuzz {
     public String calculate(int number) {
-        if(number == 4)
-            return "4";
-        if(number == 2)
-            return "2";
-        return "1";
+        return String.valueOf(number);
     }
 }
